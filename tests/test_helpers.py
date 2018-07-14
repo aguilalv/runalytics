b35c947f1b9114813efa91b5f54423bd4f876117
Alvaro Aguilar <alvaro.aguilar.lopez@gmail.com>
2018-07-14 22:45:21 +0100

    Added first test with a fail to finish the test

---
+++ tests/test_helpers.py
@@ -0,0 +1,9 @@
+import pytest
+
+import runalytics.helpers
+
+class TestGetUserTokens(object):
+    """Tests for get_user_tokens helper method"""
+
+    def test_raise_exception_if_no_token_environment_variable(self):
+        pytest.fail('Finish this test')
